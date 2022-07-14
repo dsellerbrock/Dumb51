b0c4733ab05e9f1631410ecbd09223176bcb67e7
Daniel Ellerbrock <dsellerbrock@gmail.com>
2022-07-14 11:42:14 -0500

    Beta  ALU Completed Not Tested

--- src/main/scala/Dumb51/ALU.scala
+++ src/main/scala/Dumb51/ALU.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 class ALU(size: Int) extends Module {
   object Types {
     val nop :: movi :: movr :: add :: andar :: orar :: xorar :: swapa :: clrc :: setc :: Nil
-    = Enum (12)
+    = Enum (10)
   }
   val io = IO(new Bundle{
   val op = Input(UInt(size.W))
@@ -35,6 +35,25 @@ class ALU(size: Int) extends Module {
     is(Types.add) {
       res := aux + acc + c
     }
+    is(Types.andar){
+      res:= aux & acc
+    }
+    is(Types.orar){
+      res:= aux & acc
+    }
+    is(Types.xorar){
+      res:= aux ^ acc
+    }
+    is(Types.swapa){
+      res:= Cat(acc(7,4),acc(3,0))
+    }
+    is(Types.clrc){
+      resc:= 0.S(1)
+    }
+    is(Types.setc){
+      resc:= 1.S(1)
+    }
+
   }
   when (res =/= 0.S){
     resz:=0.S
